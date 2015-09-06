49cd593a760ad4034264042adb3fecb2bc641d1f
erinkodama <erinkodama@gmail.com>
2015-09-06 16:27:24 -0700

    mobile version tweaks finish up mobile menu hide/reveal

--- js/custom.js
+++ js/custom.js
@@ -22,12 +22,28 @@ $( document ).ready(function() {
 	/* Mobile Menu */
 	function checkNav(){
 		if( $('#mobile-menu-icon').css('display') == 'block' ){
-			$('#resume-item').appendTo('#portfolio-item .submenu');
+			$('#resume-item').appendTo('#portfolio-item .submenu'); // Move the resume link to the menu dropdown
 		} else {
-			$('#resume-item').appendTo('.portfolio-menu');
+			$('#resume-item').appendTo('.portfolio-menu'); // Move the resume link back up to the main nav bar
+			$('#header .nav ul li ul').show();
 		}
 	}
 	// call the function on dom ready and on resize
 	checkNav();
 	$(window).resize(checkNav);
+	
+	// mobile menu controls
+	$('#mobile-menu-icon').click(function(){
+		if($(this).hasClass('open')){
+			$(this).find('i').removeClass('fa-chevron-up').addClass('fa-chevron-down');
+			$(this).removeClass('open');
+			$('#header .nav ul li ul').hide();			
+		}else {
+			$(this).find('i').removeClass('fa-chevron-down').addClass('fa-chevron-up');
+			$(this).addClass('open');
+			$('#header .nav ul li ul').show();
+		}
+	});
+
+
 });
